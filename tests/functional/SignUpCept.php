0545f38a60136db9cc2038bad26a36bdeff97185
Daniel Hollands <daniel@limeblast.co.uk>
2014-07-09 20:58:59 +0100

    Added user registration - now to refactor it and make it awesome.

--- tests/functional/SignUpCept.php
+++ tests/functional/SignUpCept.php
@@ -15,7 +15,10 @@ $I->click('Sign Up');
 
 $I->seeCurrentUrlEquals('');
 $I->see('Welcome to Larabook!');
+
 $I->seeRecord('users', [
 	'username' => 'JohnDoe',
 	'email'    => 'john@example.com',
 ]);
+
+$I->assertTrue(Auth::check());
