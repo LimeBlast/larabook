9d666a28a6d0ee9702e4a279696d540068292e18
Daniel Hollands <daniel@limeblast.co.uk>
2014-07-21 18:08:51 +0100

    Added a guest filter to the registration page, added a $currentUser variable to all the views, and added/removed some links according to logged in status

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -6,6 +6,8 @@
 
 		<p>Welcome to the premier place to talk about Laravel with others. Why don't you sign up to see what all the fuss is about?</p>
 
-		{{ link_to_route('register_path', 'Sign Up!', null, ['class' => 'btn btn-lg btn-primary']) }}
+		@if ( ! $currentUser)
+			{{ link_to_route('register_path', 'Sign Up!', null, ['class' => 'btn btn-lg btn-primary']) }}
+		@endif
 	</div>
 @stop
