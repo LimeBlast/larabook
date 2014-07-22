522b5659a22c607c658ff6ffe8b635ebf27caba5
Daniel Hollands <daniel@limeblast.co.uk>
2014-07-22 18:07:16 +0100

    Wrote some Integration Tests :)

--- app/Larabook/Statuses/StatusRepository.php
+++ app/Larabook/Statuses/StatusRepository.php
@@ -4,6 +4,13 @@ use Larabook\Users\User;
 
 class StatusRepository {
 
+	/**
+	 * Get all statuses associated with a user
+	 *
+	 * @param User $user
+	 *
+	 * @return mixed
+	 */
 	public function getAllForUser(User $user)
 	{
 		return $user->statuses()->get();
